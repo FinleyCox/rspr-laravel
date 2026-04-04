a3bf1a394a4ad2b6a3aa6d52d349ed791a36194d
FinleyCox <111890705+FinleyCox@users.noreply.github.com>
2026-04-04 13:16:16 +0900

    fix: 命名規則に沿わせる、ジョブの作成

--- laravel/routes/console.php
+++ laravel/routes/console.php
@@ -2,7 +2,11 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+// worksの自動同期ジョブを1時間ごとに実行する（必要に応じて適宜 ->everyMinute() や ->daily() に変更可能）
+Schedule::command('works:sync')->hourly();
